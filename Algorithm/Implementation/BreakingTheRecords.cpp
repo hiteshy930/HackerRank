1a39f0ad682c35c3761dfe1bd65a20953afc687d
HITESH <38121030+hiteshy930@users.noreply.github.com>
2020-12-21 10:42:15 +0530

    Create BreakingTheRecords.cpp

---
+++ Algorithm/Implementation/BreakingTheRecords.cpp
@@ -0,0 +1,154 @@
+/*
+Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there.
+
+For example, assume her scores for the season are represented in the array arr= [12,24,10,24]
+
+. Scores are in the same order as the games played. She would tabulate her results as follows:
+
+                                 Count
+Game  Score  Minimum  Maximum   Min Max
+ 0      12     12       12       0   0
+ 1      24     12       24       0   1
+ 2      10     10       24       1   1
+ 3      24     10       24       1   1
+
+Given the scores for a season, find and print the number of times Maria breaks her records for most and least points scored during the season.
+
+Function Description
+
+Complete the breakingRecords function in the editor below. It must return an integer array containing the numbers of times she broke her records. Index 0
+is for breaking most points records, and index 1
+
+is for breaking least points records.
+
+breakingRecords has the following parameter(s):
+
+    scores: an array of integers
+
+Input Format
+
+The first line contains an integer n
+, the number of games.
+The second line contains space-separated integers  n[i] 
+
+.
+
+
+Output Format
+
+Print two space-seperated integers describing the respective numbers of times the best (highest) score increased and the worst (lowest) score decreased.
+
+Sample Input 0
+
+9
+10 5 20 20 4 5 2 25 1
+
+Sample Output 0
+
+2 4
+
+
+
+
+*/
+
+=====================================================================================
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the breakingRecords function below.
+vector<int> breakingRecords(vector<int> a) {
+      int min_score=0,max_score=0,min_count=0,max_count;
+      vector <int> temp;
+      for(int i=0;i<a.size();i++)
+      {
+          if(i==0){
+              min_score=a[i];
+              max_score=a[i];
+          }else{
+              if(min_score > a[i]){
+                  min_score=a[i];
+                  min_count++;
+              }
+              if(max_score < a[i]){
+                  max_score=a[i];
+                  max_count++;
+              }
+          }
+      } 
+      temp.push_back(max_count);
+      temp.push_back(min_count);
+       
+       return temp;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string scores_temp_temp;
+    getline(cin, scores_temp_temp);
+
+    vector<string> scores_temp = split_string(scores_temp_temp);
+
+    vector<int> scores(n);
+
+    for (int i = 0; i < n; i++) {
+        int scores_item = stoi(scores_temp[i]);
+
+        scores[i] = scores_item;
+    }
+
+    vector<int> result = breakingRecords(scores);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
