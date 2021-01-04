bc1fdc75b28a023b5d72659e2605c11aa2327f62
HITESH <38121030+hiteshy930@users.noreply.github.com>
2021-01-04 18:26:34 +0530

    Create 2D_array_ArcGlassPattern_inMAtrix.cpp

---
+++ Misc/2D_array_ArcGlassPattern_inMAtrix.cpp
@@ -0,0 +1,114 @@
+============================================= Finding all arc glass patterns sum in a matrix and print max one ==================================
+
+Given a  'A' 2D Array, 6 x 6 :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in A to be a subset of values with indices falling in this pattern in A's graphical representation:
+
+a b c
+  d
+e f g
+There are 16  hourglasses in A , and an hourglass sum is the sum of an hourglass' values.
+
+Task
+Calculate the hourglass sum for every hourglass in A , then print the maximum hourglass sum.
+
+Example
+
+In the array shown above, the maximum hourglass sum is 7 for the hourglass in the top left corner.
+
+Input Format
+
+There are 6 lines of input, where each line contains  6 space-separated integers that describe the 2D Array  A .
+
+Constraints
+ 0<= A[i][j] <=9
+ 0<= i, j <=5
+ 
+Output Format
+
+Print the maximum hourglass sum in .
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+ contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum () is:
+
+2 4 4
+  2
+1 2 4
+============================================================================================================
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+     int max_sum = 0;
+    int temp_sum;
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            temp_sum = 0;
+            // top row
+            temp_sum += arr[i][j];
+            temp_sum += arr[i][j+1];
+            temp_sum += arr[i][j+2];
+            //middle 
+            temp_sum += arr[i+1][j+1];
+            //bottom row
+            temp_sum += arr[i+2][j];
+            temp_sum += arr[i+2][j+1];
+            temp_sum += arr[i+2][j+2];
+            
+            //if first hourglass, set as max
+            if(temp_sum > max_sum || i == 0 && j == 0)
+                max_sum = temp_sum;
+        }
+    }
+    cout << max_sum;
+    return 0;
+}
